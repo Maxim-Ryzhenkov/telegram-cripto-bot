57d6e05be8b10716342d4fe644608ce12233db79
Maxim Ryzhenkov <mryzhenkov@artec-group.com>
2022-04-26 00:02:37 +0300

    start work on dialog mode

--- app.py
+++ app.py
@@ -10,37 +10,37 @@ from extensions import ApiException
 bot = telebot.TeleBot(config.TOKEN)
 
 
-@bot.message_handler(commands=['help', 'start'])
-def handler_menu_help(message):
-    """ Обработать сообщения, содержащие команды '/start' или '/help'. """
+@bot.message_handler(commands=['start', 'menu'])
+def handler_menu_help(message: telebot.types.Message):
+    """ Обработать сообщения, содержащие команды '/start'. """
     text = f"Привет, {message.chat.first_name}."
     text = f"{text}\nЯ умею конвертировать одну валюту в другую, по актуальному курсу."
     bot.reply_to(message=message, text=text)
-    bot.send_message(chat_id=message.chat.id, text=f"Формат запроса /how_to_convert\n"
-                                                   f"Названия основных валют /major_values\n"
+    bot.send_message(chat_id=message.chat.id, text=f"Формат запроса /help\n"
+                                                   f"Названия основных валют /values\n"
                                                    f"Посмотреть полный список валют /all_values")
 
 
 @bot.message_handler(commands=['all_values', ])
-def handler_all_values(message):
+def handler_all_values(message: telebot.types.Message):
     """ Обработчик команды '/all_values' выведет полный список доступных валют. """
     bot.send_message(message.chat.id, text=f"Все доступные валюты:\n"
                                            f"{Currency.printable_string()}\n"
                                            f"...\n"
-                                           f"Формат запроса /how_to_convert")
+                                           f"Формат запроса /help")
 
 
-@bot.message_handler(commands=['major_values', ])
-def handler_major_values(message):
+@bot.message_handler(commands=['values', ])
+def handler_major_values(message: telebot.types.Message):
     """ Обработчик команды '/major_values' выведет список основных валют. """
     bot.reply_to(message, text=f"{Currency.printable_string(length=5)}\n"
                                f"...\n"
                                f"Полный список валют /all_values\n"
-                               f"Формат запроса /how_to_convert")
+                               f"Формат запроса /help")
 
 
-@bot.message_handler(commands=['how_to_convert', ])
-def handler_major_values(message):
+@bot.message_handler(commands=['help', ])
+def handler_major_values(message: telebot.types.Message):
     """ Обработчик команды '/how_to_convert' выведет подсказку по формату конвертации. """
     bot.send_message(message.chat.id, text=f"Формат команды:\n"
                                            f"<сумма> <валюта_1> <в/to> <валюте_2>\n"
@@ -54,20 +54,43 @@ def handler_major_values(message):
 
 
 @bot.message_handler()
-def request_handler(message):
+def request_handler(message: telebot.types.Message):
     """ Обработчик запроса на конвертацию. """
+    print_out_result(message.text, message.chat.id)
+
+
+def print_out_result(message: str, chat_id):
+    """ Распарсить строку и вывести результат или сообщение об ошибке. """
     try:
-        user_message = UserRequest(message)
+        from_currency, to_currency, amount = UserRequest.parse_message(message)
+        result = UserRequest.get_price(from_currency, to_currency, amount)
     except ValueError as e:
-        bot.send_message(message.chat.id, text=f"{e}")
+        bot.send_message(chat_id, text=f"{e}")
     except ApiException as e:
-        bot.send_message(message.chat.id, text=f"{e}")
+        bot.send_message(chat_id, text=f"{e}")
     else:
-        bot.send_message(message.chat.id,
-                         text=f"{user_message.amount} "
-                              f"{user_message.from_currency.correct_print_name(user_message.amount)} = "
-                              f"{user_message.result} "
-                              f"{user_message.to_currency.correct_print_name(user_message.result)}")
+        bot.send_message(chat_id, text=f"{amount} {from_currency.correct_print_name(amount)} = "
+                                       f"{result} {to_currency.correct_print_name(result)}")
+
+
+@bot.message_handler(commands=["convert", ])
+def from_currency_handler(message: telebot.types.Message):
+    from_currency = message.text.strip()
+    bot.send_message(message.chat.id, text="Выберите валюту, из которой конвертировать.")
+    bot.register_next_step_handler(message, to_currency_handler, from_currency)
+
+
+def to_currency_handler(message: telebot.types.Message, from_currency: str):
+    to_currency = message.text.strip()
+    bot.send_message(message.chat.id, text="Выберите валюту, в которую конвертировать.")
+    bot.register_next_step_handler(message, amount_handler, from_currency, to_currency)
+
+
+def amount_handler(message: telebot.types.Message, from_currency: str, to_currency: str):
+    amount = message.text.strip()
+    bot.send_message(message.chat.id, text="Введите сумму для конвертации.")
+    user_message = f"{amount} {from_currency} в {to_currency}"
+    print_out_result(user_message, message.chat.id)
 
 
 bot.polling(none_stop=True)
--- extensions.py
+++ extensions.py
@@ -18,21 +18,11 @@ class UserRequest:
     """
     pattern = r"^\s*([0-9]+[\.|,]?[0-9]*)\s+([a-zA-Zа-яА-Я]+\s*[a-zA-Zа-яА-Я]*)\s+в\s+([a-zA-Zа-яА-Я]+\s*[a-zA-Zа-яА-Я]*)\s*$"
 
-    def __init__(self, message):
-        self.message = message.text
-
-        self.amount = None
-        self.from_currency = None
-        self.to_currency = None
-        self.result = None
-
-        self._parse()
-        self._get_price()
-
-    def _parse(self):
+    @staticmethod
+    def parse_message(message: str) -> tuple:
         """ Распарсить пользовательское сообщение. """
-        print(f"Пробую парсить строку {self.message}")
-        result = re.match(UserRequest.pattern, self.message)
+        print(f"Пробую парсить строку {message}")
+        result = re.match(UserRequest.pattern, message)
         try:
             amount, from_currency, to_currency = result.groups()
         except AttributeError:
@@ -40,17 +30,19 @@ class UserRequest:
                              "Пример правильного ввода /how_to_convert\n"
                              "Названия валют /all_values")
         print(f"amount={amount}, from_currency={from_currency}, to_currency={to_currency}")
-        self.amount = float(amount.replace(",", "."))
-        if self.amount <= 0:
+        amount = float(amount.replace(",", "."))
+        if amount <= 0:
             raise ValueError("Сумма должна быть больше нуля.")
-        if self.amount > 9999999999:
+        if amount > 9999999999:
             raise ValueError("Число суммы не должна превышать 9999999999.")
-        self.from_currency = Currency.detect(from_currency)
-        self.to_currency = Currency.detect(to_currency)
+        from_currency = Currency.detect(from_currency)
+        to_currency = Currency.detect(to_currency)
+        return from_currency, to_currency, amount
 
-    def _get_price(self):
+    @staticmethod
+    def get_price(from_currency: Currency, to_currency: Currency, amount: float) -> float:
         """ Выполнить запрос на сервис через API """
-        request_string = f"https://min-api.cryptocompare.com/data/price?fsym={self.from_currency.name}&tsyms={self.to_currency.name}"
+        request_string = f"https://min-api.cryptocompare.com/data/price?fsym={from_currency.name}&tsyms={to_currency.name}"
         response = requests.get(request_string)
         err_msg = f"К сожалению, у нас технический сбой. Сервис временно недоступен. :("
         if response.status_code != 200:
@@ -59,5 +51,5 @@ class UserRequest:
             data = json.loads(response.content)
         except json.JSONDecodeError:  # если ответ не сериализуется в JSON
             raise ApiException(err_msg)
-        result = data.get(self.to_currency.name) * self.amount
-        self.result = round(result, 2)
+        result = data.get(to_currency.name) * amount
+        return round(result, 2)
